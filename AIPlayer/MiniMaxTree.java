12b08a31cd5de5e2cf317faa91296f05c4626838
Will Spurgeon <wespurgeon@wpi.edu>
2016-01-20 11:25:47 -0500

    Set up the base MiniMax class.

---
+++ AIPlayer/MiniMaxTree.java
@@ -0,0 +1,26 @@
+package AIPlayer;
+
+import java.util.ArrayList;
+
+import referee.Board;
+
+public class MiniMaxTree {
+	Board gameBoard;
+	
+	ArrayList<MiniMaxTree> children;
+	
+	/**
+	 * 
+	 * @return The value of the MiniMaxTree
+	 */
+	public int getUtilityOfRootBoard(){
+		//TODO: Implement heuristic here.
+		return 0;
+	}
+	
+	public int performMiniMaxSearch(){
+		//TODO: Generate children and perform search.
+		return 0;
+	}
+	
+}
