2c97f569211054022b98b77d6d19c2826f35c888
Will Spurgeon <wespurgeon@wpi.edu>
2016-01-20 14:47:06 -0500

    Set up information is read in.

--- referee/TestPlayer.java
+++ referee/TestPlayer.java
@@ -9,9 +9,10 @@ import java.util.List;
 
 public class TestPlayer {
 
-	String playerName="aa";
+	String playerName="Bobert";
 	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	boolean first_move=false;
+	int firstPlayer = 0;
 	
 	public void processInput() throws IOException{	
 	
@@ -26,9 +27,22 @@ public class TestPlayer {
 		}
 		else if(ls.size()==5){          //ls contains game info
 			System.out.println("0 1");  //first move
+			
+			//height col N playerThatGoesFirst timeLimit
+			int boardHeight = Integer.parseInt(ls.get(0));
+			int boardWidth = Integer.parseInt(ls.get(1));
+			int nNum = Integer.parseInt(ls.get(2));
+			firstPlayer = Integer.parseInt(ls.get(3));
+			int timeLimit = Integer.parseInt(ls.get(4));
+			
 		}
 		else if(ls.size()==4){		//player1: aa player2: bb
 			//TODO combine this information with game information to decide who is the first player
+			if(ls.get(1).equals(playerName)){
+				first_move = (firstPlayer == 1) ? true : false;
+			}else{
+				first_move = (firstPlayer == 2) ? true : false;
+			}
 		}
 		else
 			System.out.println("not what I want");
