32f0585d29325cf411eea54bc9a2d992ce1cfbd8
Will Spurgeon <wespurgeon@wpi.edu>
2016-01-20 14:59:59 -0500

    The player number information is read in and stored. The game board is added to after the opponent makes a move.

--- referee/TestPlayer.java
+++ referee/TestPlayer.java
@@ -13,13 +13,32 @@ public class TestPlayer {
 	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	boolean first_move=false;
 	int firstPlayer = 0;
+	int playerNum = 0;
+	int opponentNum = 0;
+	
+	Board gameBoard;
 	
 	public void processInput() throws IOException{	
-	
+		
     	String s=input.readLine();	
 		List<String> ls=Arrays.asList(s.split(" "));
+		//Add the last move to our game board.
 		if(ls.size()==2){
-			System.out.println(ls.get(0)+" "+ls.get(1));
+			//System.out.println(ls.get(0)+" "+ls.get(1));
+			
+			if(Integer.parseInt(ls.get(1)) == 0){
+				//POP out
+				gameBoard.removeADiscFromBottom(Integer.parseInt(ls.get(0)));
+			}else{
+				//Drop
+				gameBoard.dropADiscFromTop(Integer.parseInt(ls.get(0)), opponentNum);
+			}
+			
+			//TODO: implement everything here.
+			//Perform MiniMax and pruning using heuristic. 
+			//Return a move within the time limit.
+			
+			
 		}
 		else if(ls.size()==1){
 			System.out.println("game over!!!");
@@ -35,12 +54,18 @@ public class TestPlayer {
 			firstPlayer = Integer.parseInt(ls.get(3));
 			int timeLimit = Integer.parseInt(ls.get(4));
 			
+			gameBoard = new Board(boardHeight, boardWidth, nNum);
+			
 		}
 		else if(ls.size()==4){		//player1: aa player2: bb
 			//TODO combine this information with game information to decide who is the first player
 			if(ls.get(1).equals(playerName)){
+				playerNum = 1;
+				opponentNum = 2;
 				first_move = (firstPlayer == 1) ? true : false;
 			}else{
+				playerNum = 2;
+				opponentNum = 1;
 				first_move = (firstPlayer == 2) ? true : false;
 			}
 		}
