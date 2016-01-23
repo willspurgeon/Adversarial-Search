d64d56705cb2cd061915bf1bf1e45f2068ceb09c
danielpongratz <danielpongratz@palinka.wifi.wpi.edu>
2016-01-23 17:03:22 -0500

    Made an array

--- AIPlayer/MiniMaxTree.java
+++ AIPlayer/MiniMaxTree.java
@@ -7,80 +7,84 @@ import referee.Board;
 public class MiniMaxTree {
 	Board gameBoard;
 	boolean thisIsMax;
-	
+
 	ArrayList<MiniMaxTree> children;
-	
-	
-	MiniMaxTree(Board board, boolean rootIsMax){
+
+	MiniMaxTree(Board board, boolean rootIsMax) {
 		this.gameBoard = board;
 		this.thisIsMax = rootIsMax;
 	}
-	
+
 	/**
 	 * 
 	 * @return The value of the MiniMaxTree
 	 */
-	public int getUtilityOfRootBoard(){
-		//TODO: Implement heuristic here.
+	public int getUtilityOfRootBoard() {
+		// TODO: Implement heuristic here.
 		return 0;
 	}
-	
-	public int performMiniMaxSearch(){
-		//TODO: Generate children and perform search.
+
+	public int performMiniMaxSearch() {
+		// TODO: Generate children and perform search.
 		int result;
-		if(thisIsMax){
+		if (thisIsMax) {
 			result = Integer.MIN_VALUE;
-		
-			for(MiniMaxTree child: children){
+
+			for (MiniMaxTree child : children) {
 				int thisChildsNum = child.performMiniMaxSearch();
-				if(thisChildsNum > result){
+				if (thisChildsNum > result) {
 					result = thisChildsNum;
 				}
 			}
-		}else{
+		} else {
 			result = Integer.MAX_VALUE;
-			
-			for(MiniMaxTree child: children){
+
+			for (MiniMaxTree child : children) {
 				int thisChildsNum = child.performMiniMaxSearch();
-				if(thisChildsNum < result){
+				if (thisChildsNum < result) {
 					result = thisChildsNum;
 				}
 			}
 		}
-		
+
 		return result;
 	}
-	
-	public void buildTreeToDepth(int i){
-		if(i < 1){
+
+	public void buildTreeToDepth(int i) {
+		if (i < 1) {
 			return;
 		}
-		int playerturn=-1;
-		for(int j = 0; j < gameBoard.width; j++){
+		int playerturn = -1;
+
+		ArrayList<Board> Boardarr = new ArrayList<Board>();
+
+		for (int j = 0; j < gameBoard.width; j++) {
 			playerturn++;
-			//Build all possible boards for drop.
-			Board Temp = new Board( gameBoard.height, gameBoard.width, gameBoard.getN());
-			Temp.dropADiscFromTop(j, playerturn%2);
-		
+			// Build all possible boards for drop.
+
+			Board Temp = new Board(gameBoard.height, gameBoard.width, gameBoard.getN());
+
+			Temp.dropADiscFromTop(j, playerturn % 2);
+
+			Boardarr.add(Temp);
 		}
-		
-		for(int j = 0; j < gameBoard.width; j++){
-			//Build all possible boards for pop.
+
+		for (int j = 0; j < gameBoard.width; j++) {
+			// Build all possible boards for pop.
+
 			playerturn++;
-			Board Temp = new Board( gameBoard.height, gameBoard.width, gameBoard.getN());
-			if(Temp.canRemoveADiscFromBottom(j, playerturn%2))
-			{	
+
+			Board Temp = new Board(gameBoard.height, gameBoard.width, gameBoard.getN());
+			if (Temp.canRemoveADiscFromBottom(j, playerturn % 2)) {
 				Temp.removeADiscFromBottom(j);
+				Boardarr.add(Temp);
 			}
-		
-			
+
 		}
-		
-		for(int j = 0; j < gameBoard.width; j++){
-			buildTreeToDepth(i-1);
+		for (int j = 0; j < gameBoard.width; j++) {
+			buildTreeToDepth(i - 1);
 		}
-		
-		
+
 	}
-	
+
 }
